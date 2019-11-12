ca35df82f8f85abd60761a6e856354826eccbbfd
bs65 <bharathkumarkarthick@gmail.com>
2019-11-12 09:15:31 +0530

    web table

--- Test/Pages/Banking_pages.js
+++ Test/Pages/Banking_pages.js
@@ -2,26 +2,57 @@ const by = require('protractor').by
 const element = require('protractor').element
 
 const loc = {
-    user: element(by.cssContainingText('option', 'Ron Weasly')),
-    Login: element(by.buttonText('Login')),
-    Logout: element(by.buttonText('Logout')),
-    Deposit: element(by.buttonText('Deposit')),
-    balance: element(by.binding('amount')),
-    Deposit_amount: element(by.xpath('//*[text()="Deposit"]')),
-    input_amount: element(by.model('amount')),
-    alert:element(by.cssContainingText('span','Deposit Successful'))
+    customer_login: element(by.css('[ng-Click="customer()"]')),
+    manager_login: element(by.css('[ng-click="manager()"]')),
+    customer: {
+        user: element(by.cssContainingText('option', 'Ron Weasly')),
+        Login: element(by.buttonText('Login')),
+        Logout: element(by.buttonText('Logout')),
+        Deposit: element(by.buttonText('Deposit')),
+        balance: element(by.binding('amount')),
+        Deposit_amount: element(by.xpath('//*[text()="Deposit"]')),
+        input_amount: element(by.model('amount')),
+        alert: element(by.cssContainingText('span', 'Deposit Successful')),
+        account: element(by.binding('account')),
+    },
+    manager: {
+        addCustomer: element(by.css('[ng-click="addCust()"]')),
+        openAccount: element(by.css('[ng-click="openAccount()"]')),
+        listCustomers: {
+            btn: element(by.css('[ng-click="showCust()"]')),
+            table: element.all(by.repeater('cust in Customers | orderBy:sortType:sortReverse | filter:searchCustomer'))
+        }
+    }
+
 }
 exports.loc = loc;
 
 const lib = {
     login: () => {
-        loc.user.click();
-        loc.Login.click();
+        loc.customer_login.click();
+        loc.customer.user.click();
+        loc.customer.Login.click();
     },
-    deposit: () => {
-        loc.Deposit.click();
-        loc.input_amount.sendKeys('1000');
-        loc.Deposit_amount.click();
+    manager_login: () => {
+        loc.manager_login.click();
+    },
+    deposit: (e) => {
+        loc.customer.Deposit.click();
+        loc.customer.input_amount.sendKeys(e);
+        loc.customer.Deposit_amount.click();
+    },
+    countCustomers: async () => {
+        loc.manager.listCustomers.btn.click()
+        let rows = await loc.manager.listCustomers.table.count();
+        return rows;
+    },
+    getAllCustomers: async () => {
+        loc.manager.listCustomers.btn.click()
+        let rows = loc.manager.listCustomers.table.get(0);
+        let cells = rows.$$('td');
+        let data = await cells.get(0).getText();
+        console.log(data);
+        return data;
     },
     waitElementDisplayed(element) {
         return browser.wait(() => {
--- Test/Specs/Banking.js
+++ Test/Specs/Banking.js
@@ -1,11 +1,11 @@
 // http://www.way2automation.com/angularjs-protractor/banking/#/customer
-const loc  = require('../Pages/Banking_pages').loc;
+const loc = require('../Pages/Banking_pages').loc;
 const libs = require('../Pages/Banking_pages').lib
 
 describe('Simple tests', () => {
 
     beforeEach(() => {
-        browser.driver.get('http://www.way2automation.com/angularjs-protractor/banking/#/customer');
+        browser.driver.get('http://www.way2automation.com/angularjs-protractor/banking/#/login');
     });
 
     it('first test - Check for title', () => {
@@ -13,16 +13,36 @@ describe('Simple tests', () => {
     });
 
     it('first test - Login successfull', () => {
-        libs.login()        
-        expect(loc.Logout.isDisplayed()).toEqual(true);
+        libs.login()
+        expect(loc.customer.Logout.isDisplayed()).toEqual(true);
     });
 
-    it('Deposit amount - Check for title', () => {
-        libs.login()        
-        expect(loc.Logout.isDisplayed()).toEqual(true);
-        libs.deposit()
-        expect(loc.alert.isDisplayed()).toEqual(true);
-        expect(loc.balance.getText()).toEqual('1000');
+    it('Deposit amount - Check for title', async () => {
+        let amount = '1000';
+        libs.login();
+        expect(loc.customer.Logout.isDisplayed()).toEqual(true);
+        libs.deposit(amount);
+        expect(loc.customer.alert.isDisplayed()).toEqual(true);
+        expect(loc.customer.balance.getText()).toEqual(amount);
+        let a = await loc.customer.account.getText();
+        console.log(a);
+    });
+
+    it('Check webtable - list customers - banking', async () => {
+        libs.manager_login();
+        expect(loc.manager.addCustomer.isDisplayed()).toEqual(true)
+        expect(loc.manager.listCustomers.btn.isDisplayed()).toEqual(true)
+        expect(loc.manager.openAccount.isDisplayed()).toEqual(true)
+        expect(libs.countCustomers()).toEqual(5)
+    });
+
+    it('Check webtable - list customers - banking', async () => {
+        libs.manager_login();
+        expect(loc.manager.addCustomer.isDisplayed()).toEqual(true)
+        expect(loc.manager.listCustomers.btn.isDisplayed()).toEqual(true)
+        expect(loc.manager.openAccount.isDisplayed()).toEqual(true)
+        expect(libs.getAllCustomers()).toEqual('Hermoine');
+
     });
 
 })
